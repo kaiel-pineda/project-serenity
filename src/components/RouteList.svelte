cdc7af9f7128e953c483d187164a0023786ca40c
Karl Pineda <29784789+kaiel-pineda@users.noreply.github.com>
2023-12-21 20:24:22 -0800

    Update RouteList.svelte

--- src/components/RouteList.svelte
+++ src/components/RouteList.svelte
@@ -1,29 +1,27 @@
 <script>
 	let newItem = "";
-
-	// Initialize todoList with a default value or from localStorage
 	let todoList = [];
+
 	if (typeof window !== "undefined") {
 		const savedList = localStorage.getItem("todoList");
-		if (savedList) {
-			todoList = JSON.parse(savedList);
-		}
+		todoList = savedList ? JSON.parse(savedList) : [];
 	}
 
 	function addToList() {
-		todoList = [...todoList, { text: newItem, status: false }];
+		todoList = [...todoList, { id: new Date().getTime(), text: newItem, status: false }];
 		newItem = "";
 		updateLocalStorage();
 	}
 
-	function removeFromList(index) {
-		todoList.splice(index, 1);
-		todoList = todoList;
+	function removeFromList(itemId) {
+		todoList = todoList.filter((item) => item.id !== itemId);
 		updateLocalStorage();
+		checkCurrentPage();
 	}
 
-	function updateItemStatus(index, status) {
-		todoList[index].status = status;
+	function updateItemStatus(paginatedIndex, status) {
+		const actualIndex = (currentPage - 1) * itemsPerPage + paginatedIndex;
+		todoList[actualIndex].status = status;
 		updateLocalStorage();
 	}
 
@@ -37,26 +35,54 @@
 			localStorage.setItem("todoList", JSON.stringify(todoList));
 		}
 	}
+
+	function checkCurrentPage() {
+		totalPages = Math.ceil(todoList.length / itemsPerPage);
+		currentPage = currentPage > totalPages ? Math.max(1, totalPages) : currentPage;
+	}
+
+	let showInput = true;
+	let currentPage = 1;
+	const itemsPerPage = 12;
+	let totalPages = Math.ceil(todoList.length / itemsPerPage);
+
+	$: paginatedList = todoList.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
+	$: totalPages = Math.ceil(todoList.length / itemsPerPage);
+
+	function toggleInputVisibility() {
+		showInput = !showInput;
+	}
 </script>
 
 <section class="container mx-auto px-6">
 	<div class="flex flex-col gap-y-12">
-		<div class="relative">
-			<input class="border-2 border-black rounded-md h-20 w-full p-4 text-xl" bind:value={newItem} type="text" placeholder="Create stop" />
-			<button class="absolute right-0 h-full bg-[#505050] rounded-tr-md rounded-br-md p-6" on:click={addToList}>
-				<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="fill-white h-6 w-6" viewBox="0 0 16 16">
-					<path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2" />
-				</svg>
-			</button>
+		<div class="flex flex-col gap-y-6">
+			{#if showInput}
+				<div class="relative">
+					<input class="border-2 border-black rounded-md h-20 w-full p-4 text-xl" bind:value={newItem} type="text" placeholder="Create stop" />
+					<button class="absolute right-0 h-full bg-[#505050] rounded-tr-md rounded-br-md p-6" on:click={addToList}>
+						<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="fill-white h-6 w-6" viewBox="0 0 16 16">
+							<path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2" />
+						</svg>
+					</button>
+				</div>
+			{/if}
+			<div class="self-end">
+				<button class="p-3 border border-gray-200 rounded-md bg-neutral-100" on:click={toggleInputVisibility}>{showInput ? "Hide" : "Show"}</button>
+			</div>
+		</div>
+
+		<div>
+			<span class="text-lg font-medium">{todoList.length} stops</span>
 		</div>
 
 		<div class="grid grid-cols-2 gap-y-6 gap-x-6 w-full items-center">
-			{#each todoList as item, index}
+			{#each paginatedList as item, paginatedIndex (paginatedIndex)}
 				<div class="flex flex-col border border-gray-300 rounded-md">
 					<span class="text-base font-medium text-center p-2" class:checked={item.status}>{item.text}</span>
 					<div class="flex items-center justify-between border-t border-gray-300 p-2 bg-neutral-100">
-						<input bind:checked={item.status} type="checkbox" on:change={() => updateItemStatus(index, item.status)} />
-						<button on:click={() => removeFromList(index)}>
+						<input bind:checked={item.status} type="checkbox" on:change={() => updateItemStatus(paginatedIndex, item.status)} />
+						<button on:click={() => removeFromList(item.id)}>
 							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
 								<path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
 							</svg>
@@ -66,7 +92,15 @@
 			{/each}
 		</div>
 
-		<div class="flex w-full justify-end">
+		{#if totalPages > 1}
+			<div class="pagination-controls flex items-center justify-center gap-4 mt-4">
+				<button class="p-3 border border-gray-200 rounded-md bg-neutral-100" disabled={currentPage === 1} on:click={() => (currentPage = Math.max(1, currentPage - 1))}> Previous </button>
+				<span>{currentPage} of {totalPages}</span>
+				<button class="p-3 border border-gray-200 rounded-md bg-neutral-100" disabled={currentPage === totalPages} on:click={() => (currentPage = Math.min(totalPages, currentPage + 1))}> Next </button>
+			</div>
+		{/if}
+
+		<div class="flex w-full justify-end mb-6">
 			<button class="bg-[#ceb202] rounded-md p-3 text-white font-semibold" on:click={clearAll}>Clear All</button>
 		</div>
 	</div>
