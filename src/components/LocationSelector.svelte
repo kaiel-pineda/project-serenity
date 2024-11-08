50fc2869940a8805be0fc81fbb292661552ea80e
Karl Pineda <29784789+kaiel-pineda@users.noreply.github.com>
2024-11-08 09:14:37 -0800

    Update 1.9.4

--- src/components/LocationSelector.svelte
+++ src/components/LocationSelector.svelte
@@ -196,26 +196,30 @@
                 {/if}
             {/if}
 
-            <button class="justify-self-end p-2 relative" on:click={togglePickups}>
-                <button class="absolute inset-0 size-full" {...$tooltipTrigger} use:tooltipTrigger />
-                {#if $isPickups}
-                    <svg xmlns="http://www.w3.org/2000/svg" class="size-6 fill-old-gold-500" viewBox="0 0 16 16">
-                        <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5l2.404.961L10.404 2zm3.564 1.426L5.596 5 8 5.961 14.154 3.5zm3.25 1.7-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464z" />
-                    </svg>
-                {:else}
-                    <svg xmlns="http://www.w3.org/2000/svg" class="size-6 fill-old-gold-500" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M15.528 2.973a.75.75 0 0 1 .472.696v8.662a.75.75 0 0 1-.472.696l-7.25 2.9a.75.75 0 0 1-.557 0l-7.25-2.9A.75.75 0 0 1 0 12.331V3.669a.75.75 0 0 1 .471-.696L7.443.184l.01-.003.268-.108a.75.75 0 0 1 .558 0l.269.108.01.003zM10.404 2 4.25 4.461 1.846 3.5 1 3.839v.4l6.5 2.6v7.922l.5.2.5-.2V6.84l6.5-2.6v-.4l-.846-.339L8 5.961 5.596 5l6.154-2.461z" />
-                    </svg>
-                {/if}
+            <div class="justify-self-end relative flex items-center">
+                <button class="relative" on:click={togglePickups}>
+                    <button class="absolute inset-0 size-full" {...$tooltipTrigger} use:tooltipTrigger />
+                    {#if $isPickups}
+                        <svg xmlns="http://www.w3.org/2000/svg" class="size-6 fill-old-gold-500" viewBox="0 0 16 16">
+                            <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5l2.404.961L10.404 2zm3.564 1.426L5.596 5 8 5.961 14.154 3.5zm3.25 1.7-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464z" />
+                        </svg>
+                    {:else}
+                        <svg xmlns="http://www.w3.org/2000/svg" class="size-6 fill-old-gold-500" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M15.528 2.973a.75.75 0 0 1 .472.696v8.662a.75.75 0 0 1-.472.696l-7.25 2.9a.75.75 0 0 1-.557 0l-7.25-2.9A.75.75 0 0 1 0 12.331V3.669a.75.75 0 0 1 .471-.696L7.443.184l.01-.003.268-.108a.75.75 0 0 1 .558 0l.269.108.01.003zM10.404 2 4.25 4.461 1.846 3.5 1 3.839v.4l6.5 2.6v7.922l.5.2.5-.2V6.84l6.5-2.6v-.4l-.846-.339L8 5.961 5.596 5l6.154-2.461z" />
+                        </svg>
+                    {/if}
+                </button>
+
                 {#if autoToggleEnabled}
-                    <div class="top-0 start-8 absolute flex">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="size-3 fill-old-gold-600" viewBox="0 0 16 16">
-                            <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
-                            <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0"/>
-                          </svg>
-                    </div>
+                <div class="-top-3 left-5 absolute">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="size-4 fill-old-gold-600" viewBox="0 0 16 16">
+                        <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
+                        <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0"/>
+                      </svg>
+                </div>
                 {/if}
-            </button>
+            </div>
+
 
             {#if $isTooltipOpen}
             <div
