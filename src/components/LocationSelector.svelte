f4ba01c28af75fbbdcdd1cd38baca66f55fd5e70
Karl Pineda <29784789+kaiel-pineda@users.noreply.github.com>
2024-11-08 09:04:09 -0800

    Update 1.9.3

--- src/components/LocationSelector.svelte
+++ src/components/LocationSelector.svelte
@@ -84,7 +84,6 @@
         const currentTimeInMinutes = now.getHours() * 60 + now.getMinutes();
         const targetTimeInMinutes = hour * 60 + minute;
 
-        // Toggle `isPickups` based on the time comparison
         if (currentTimeInMinutes >= targetTimeInMinutes) {
             isPickups.set(true);
         } else {
@@ -207,6 +206,14 @@
                     <svg xmlns="http://www.w3.org/2000/svg" class="size-6 fill-old-gold-500" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M15.528 2.973a.75.75 0 0 1 .472.696v8.662a.75.75 0 0 1-.472.696l-7.25 2.9a.75.75 0 0 1-.557 0l-7.25-2.9A.75.75 0 0 1 0 12.331V3.669a.75.75 0 0 1 .471-.696L7.443.184l.01-.003.268-.108a.75.75 0 0 1 .558 0l.269.108.01.003zM10.404 2 4.25 4.461 1.846 3.5 1 3.839v.4l6.5 2.6v7.922l.5.2.5-.2V6.84l6.5-2.6v-.4l-.846-.339L8 5.961 5.596 5l6.154-2.461z" />
                     </svg>
+                {/if}
+                {#if autoToggleEnabled}
+                    <div class="top-0 start-8 absolute flex">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="size-3 fill-old-gold-600" viewBox="0 0 16 16">
+                            <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
+                            <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0"/>
+                          </svg>
+                    </div>
                 {/if}
             </button>
 
